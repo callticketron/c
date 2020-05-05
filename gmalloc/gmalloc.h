77ccbcb74acb8a3e95ee16e4e1601b01f9af402d
Ticketron <ticketron@protonmail.com>
2020-05-05 13:49:29 -0500

    Base for the gmalloc thing.

--- gmalloc/gmalloc.h
+++ gmalloc/gmalloc.h
@@ -1,3 +1,10 @@
 #ifndef GMALLOC_H
-#define GMALLOC_H value
+#define GMALLOC_H
+
+typedef struct GmallocNode {
+    int references;
+    GmallocNode *next;
+    GmallocNode *prev;
+} GmallocNode;
+
 #endif /* ifndef GMALLOC_H */
